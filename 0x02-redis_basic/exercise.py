1989258d53119ff8797b6ba9c07b7df59839c99e
segunUb18 <eeomo@yahoo.com>
2023-07-20 15:24:18 +0100

    :test_tube: Test lrange in replay function
    lrange in replay function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -33,6 +33,31 @@ def call_history(method: Callable) -> Callable:
             self._redis.rpush(out_key, output)
         return output
     return wrapper
+
+def replay(method: Callable) -> None:
+    '''Displays the call history of a cache class method.
+    '''
+    if method is None or not hasattr(method, '__self__'):
+        return 
+    redis_store = getattr(method.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fn_name = method.__qualname__
+    in_key = '{}:inputs'.format(fn_name)
+    out_key= '{}:outputs'.format(fn_name)
+    fn_call_count = 0
+    if redis_store.exists(fn_name) != 0:
+        fn_call_count = int(redis_store.get(fn_name))
+    print('{} was called {} times:'.format(fn_name, fn_call_count))
+    fn_inputs = redis_store.lrange(in_key, 0, -1)
+    fn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fn_input, fn_output in zip(fn_inputs, fn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fn_name,
+            fn_input.decode("utf-8"),
+            fn_output,
+        ))
+
     
 
 class Cache():
