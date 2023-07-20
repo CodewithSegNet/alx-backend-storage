4feb510aee699b6c93b568f883dc9a14dfbe5226
segunUb18 <eeomo@yahoo.com>
2023-07-20 13:36:39 +0100

    :bug: Test exercise
    test exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,22 +20,20 @@ def count_calls(method: Callable) -> Callable:
 def call_history(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args):
+        """ normalize the input arg to a string """
+        input_args_str = [str(arg) for arg in args]
+
         """ append the input arg to the Redis list """
         inputs_key = f"{method.__qualname__}:inputs"
-        for arg in args:
-            arg_str = str(arg)
-            self._redis.rpush(inputs_key, arg_str)
-
+        self._redis.rpush(inputs_key, *input_args_str)
         """ call the original method and store its result """
         result = method(self, *args)
-
+        """ normalize the result to a string """
+        result_str = str(result)
         """ append the input arg to the Redis list """
         outputs_key = f"{method.__qualname__}:outputs"
-        result_str = str(result)
         self._redis.rpush(outputs_key, result_str)
-
         return result
-
     return wrapper
 
 
